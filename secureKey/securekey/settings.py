d6f1cf483e9b5e80ee39442b160f558e79824e42
nalini-tls <Nalini@teralumensolutions.com>
2026-08-21 21:55:32 +0530

    render link

--- secureKey/securekey/settings.py
+++ secureKey/securekey/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['securekey-8s49.onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
